65cf28f036d0f45d965929cb150145eb6f07979f
LAKSHYA CHOUDHARY <choudharylakshya.lc@gmail.com>
2026-03-09 19:54:32 +0530

    public

--- server/public/assets/AnimatedGradientBorder-Cm-ebVzk.js
+++
@@ -1,4 +0,0 @@
-import{j as a,m as r}from"./index-j4T3pWo2.js";const i=({children:e,className:n=""})=>a.jsx(r.div,{className:`
-        p-0.5
-        ${n}
-      `,style:{background:"conic-gradient(from var(--angle), #64748b80 80%, #6366f1 86%, #a5b4fc 90%, #6366f1 94%, #64748b80)"},animate:{"--angle":["0deg","360deg"]},transition:{duration:6,ease:"circInOut",repeat:1/0},children:a.jsx("div",{className:"rounded-2xl w-full h-full bg-slate-900",children:e})});export{i as A};
